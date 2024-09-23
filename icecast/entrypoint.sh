1bef0d8722d7eed89fe91853d27c8b1d9b2a126c
GiBi-develop <grixa300@yandex.ru>
2024-09-23 20:59:53 +0300

    Финальный вариант

--- icecast/entrypoint.sh
+++ icecast/entrypoint.sh
@@ -2,8 +2,23 @@
 
 set -e
 
-if [ -z "$ICECAST_SOURCE_PASSWORD_FILE" ] || [ -z "$ICECAST_RELAY_PASSWORD_FILE" ] || [ -z "$ICECAST_ADMIN_USERNAME_FILE" ] || [ -z "$ICECAST_ADMIN_PASSWORD_FILE" ]; then
-    echo "Заданы не все секреты."
+if [ -z "$ICECAST_SOURCE_PASSWORD_FILE" ]; then
+    echo "Не задан секрет ICECAST_SOURCE_PASSWORD"
+    exit 1
+fi
+
+if [ -z "$ICECAST_RELAY_PASSWORD_FILE" ]; then
+    echo "Не задан секрет ICECAST_RELAY_PASSWORD"
+    exit 1
+fi
+
+if [ -z "$ICECAST_ADMIN_USERNAME_FILE" ]; then
+    echo "Не задан секрет ICECAST_ADMIN_USERNAME"
+    exit 1
+fi
+
+if [ -z "$ICECAST_ADMIN_PASSWORD_FILE" ]; then
+    echo "Не задан секрет ICECAST_ADMIN_PASSWORD"
     exit 1
 fi
 
@@ -15,15 +30,27 @@ ICECAST_ADMIN_PASSWORD=$(cat "$ICECAST_ADMIN_PASSWORD_FILE")
 xmlstarlet ed \
     -u "/icecast/authentication/source-password" -v "${ICECAST_SOURCE_PASSWORD}" \
     -u "/icecast/authentication/relay-password" -v "${ICECAST_RELAY_PASSWORD}" \
-    -u "/icecast/admin/username" -v "${ICECAST_ADMIN_USERNAME}" \
-    -u "/icecast/admin/password" -v "${ICECAST_ADMIN_PASSWORD}" \
+    -u "/icecast/authentication/admin-user" -v "${ICECAST_ADMIN_USERNAME}" \
+    -u "/icecast/authentication/admin-password" -v "${ICECAST_ADMIN_PASSWORD}" \
     /etc/icecast2/icecast.xml > /tmp/icecast.xml
 
-if [ -z "$(xmlstarlet sel -t -v '/icecast/authentication/source-password' /etc/icecast2/icecast.xml 2>/dev/null)" ] ||
-   [ -z "$(xmlstarlet sel -t -v '/icecast/authentication/relay-password' /etc/icecast2/icecast.xml 2>/dev/null)" ] ||
-   [ -z "$(xmlstarlet sel -t -v '/icecast/admin/username' /etc/icecast2/icecast.xml 2>/dev/null)" ] ||
-   [ -z "$(xmlstarlet sel -t -v '/icecast/admin/password' /etc/icecast2/icecast.xml 2>/dev/null)" ]; then
-    echo "Заданы не все секреты."
+if [ -z "$(xmlstarlet sel -t -v '/icecast/authentication/source-password' /tmp/icecast.xml 2>/dev/null)" ]; then
+    echo "Неверно передан ICECAST_SOURCE_PASSWORD"
+    exit 1
+fi
+
+if [ -z "$(xmlstarlet sel -t -v '/icecast/authentication/relay-password' /tmp/icecast.xml 2>/dev/null)" ]; then
+    echo "Неверно передан ICECAST_RELAY_PASSWORD"
+    exit 1
+fi
+
+if [ -z "$(xmlstarlet sel -t -v '/icecast/authentication/admin-user' /tmp/icecast.xml 2>/dev/null)" ]; then
+    echo "Неверно передан ICECAST_ADMIN_USERNAME"
+    exit 1
+fi
+
+if [ -z "$(xmlstarlet sel -t -v '/icecast/authentication/admin-password' /tmp/icecast.xml 2>/dev/null)" ]; then
+    echo "Неверно передан ICECAST_ADMIN_PASSWORD"
     exit 1
 fi
 
